7c35f4d5e7380d239d5e5db32bcc5e0815152692
stas <astatsegre@gmail.com>
2019-09-22 00:39:19 +0300

    handle ping and new session

--- index.js
+++ index.js
@@ -10,24 +10,25 @@ module.exports = async (req, res) => {
   const stringsNumbers = [1, 2, 3, 4, 5, 6];
   const tokens = request.nlu.entities.tokens;
   const stringsInWords = {
-      1: ['тонкая', 'нижняя', 'ми первой октавы'],
-      2: ['си', 'b', 'б', 'би'],
-      3: ['соль', 'g', 'джи'],
-      4: ['ре', 'рэ', 'd', 'дэ'],
-      5: ['ля', 'a', 'а'],
-      6: ['толстая', 'верхняя', 'ми большой октавы', 'e', 'йе'],
-    };
-    const audioIds = {
-      1: '5f338760-dfd5-47c1-9ee0-8fd160bbc399.opus',
-      2: 'b36ae01e-5c72-49e7-b3dd-889889875830.opus',
-      3: '076f3ed1-a913-4f88-a18f-2020daf18bc4.opus',
-      4: '5d3b1e61-c5b1-4719-87b4-bb375cc6a79b.opus',
-      5: '7bbb29d2-aca2-4c47-bc62-b2be77a68f38.opus',
-      6: 'b4be4e3f-b464-4757-bf95-2edd1b4008c6.opus',
-    };
+    1: ['тонкая', 'нижняя', 'ми первой октавы'],
+    2: ['си', 'b', 'б', 'би'],
+    3: ['соль', 'g', 'джи'],
+    4: ['ре', 'рэ', 'd', 'дэ'],
+    5: ['ля', 'a', 'а'],
+    6: ['толстая', 'верхняя', 'ми большой октавы', 'e', 'йе'],
+  };
+  const audioIds = {
+    1: '5f338760-dfd5-47c1-9ee0-8fd160bbc399.opus',
+    2: 'b36ae01e-5c72-49e7-b3dd-889889875830.opus',
+    3: '076f3ed1-a913-4f88-a18f-2020daf18bc4.opus',
+    4: '5d3b1e61-c5b1-4719-87b4-bb375cc6a79b.opus',
+    5: '7bbb29d2-aca2-4c47-bc62-b2be77a68f38.opus',
+    6: 'b4be4e3f-b464-4757-bf95-2edd1b4008c6.opus',
+  };
   const userStringNumber = getStringNumber(request);
-    let responseIfWrongUserRequest = 'Назовите струну и я её сыграю';
+  let responseIfWrongUserRequest = 'Назовите струну и я её сыграю';
     function getStringNumber (request) {
+      if (session.new || request.original_utterance === 'ping') return null;
       const entities = request.nlu.entities;
       if (entities.length === 1 && entities[0].type === 'YANDEX.NUMBER') {
         if (stringsNumbers.includes(entities[0].value)) {
